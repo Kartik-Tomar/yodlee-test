36d519f0d72d91099fec05791aa1182d27e73c99
kartik-tomar <kartik9896@gmail.com>
2020-09-07 03:53:28 +0530

    Prepared for deployment

---
+++ client/build/precache-manifest.be6f513a03999df9224ea4de416c0891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbd965f188810fa94e685d396dbbd23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40d36c0458d093e7035",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cb30d17d847f1aa5b5f4",
+    "url": "/static/css/main.82ca38a8.chunk.css"
+  },
+  {
+    "revision": "c40d36c0458d093e7035",
+    "url": "/static/js/2.012356de.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/static/js/2.012356de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb30d17d847f1aa5b5f4",
+    "url": "/static/js/main.ebbad62d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
